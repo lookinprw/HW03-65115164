b75a2ca96d6ab9a49394b7c685d46f7b960a36af
lookinprw <140128880+lookinprw@users.noreply.github.com>
2024-08-26 02:04:50 +0700

    Create config.dart

---
+++ config.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_application_fullstack/model/users.dart';
+
+class Configure {
+  static const server = "192.168.1.42:3000";
+  static Users login = Users();
+  static List<String> gender = [
+    "None",
+    "Male",
+    "Female",
+    "Other",
+  ];
+}
